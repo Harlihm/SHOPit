b9ba0c589d9472d3892e9e603c1ac180aaf097cd
jbpain <harlihm1@gmail.com>
2023-04-26 21:06:51 +0100

    choke

--- src/components/ProductCards.jsx
+++ src/components/ProductCards.jsx
@@ -12,11 +12,13 @@ const ProductCards = ({ product }) => {
         <h2 style={{fontSize:"16px"}}> {product.title.substring(0,15)}</h2>
        
       </div>
-      <div style={{display:"flex" ,gap:"0.5rem"}}>
-       <div style={{width:"7rem",position:"relative",gap:"10px", display:"flex" ,justifyContent:"flex-end" ,fontSize: "0.875rem",lineHeight: "1.25rem"}}>
-       <p style={{color: "gray" ,textDecoration:"line-through"}}>${product.oldPrice}</p>
-        <p style={{fontWeight:"600"}}>${product.price}</p>
-      </div>
+      {/* {width:"7rem",position:"relative",gap:"10px", display:"flex" ,justifyContent:"flex-end" ,fontSize: "0.875rem",lineHeight: "1.25rem"} */}
+      <div style={{display:"flex" ,gap:"0.5rem" ,position:"relative" ,overflow:"hidden"}}>
+           <div className="prod-pp">
+            <p style={{color: "gray" ,textDecoration:"line-through"}}>${product.oldPrice}</p>
+            <p style={{fontWeight:"600"}}>${product.price}</p>
+           </div>
+        <p className="prod-p" >add to cart</p>
       </div>
      </div>
     </div>
