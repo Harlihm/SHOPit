57e46d435b3a60cf74c98b970715b2f3d937a49c
jbpain <harlihm1@gmail.com>
2023-05-09 14:31:07 +0100

    fixed banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -11,14 +11,16 @@ const [currentSlide ,setCurrentSlide]=useState(0);
     "https://m.media-amazon.com/images/I/71tIrZqybrL._SX3000_.jpg",
     "https://m.media-amazon.com/images/I/61TD5JLGhIL._SX3000_.jpg",
     "https://m.media-amazon.com/images/I/81d5OrWJAkL._SX3000_.jpg",
+    "https://images.unsplash.com/photo-1472851294608-062f824d29cc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+
 
   ]
 
   const prevSlide=()=>{
-setCurrentSlide(currentSlide===0?3: (prev)=>prev-1)
+setCurrentSlide(currentSlide===0?2: (prev)=>prev-1)
   }
   const nextSlide=()=>{
-    setCurrentSlide(currentSlide===3?0: (prev)=>prev+1)
+    setCurrentSlide(currentSlide===2?0: (prev)=>prev+1)
       }
 
       // console.log(currentSlide);
@@ -30,6 +32,7 @@ setCurrentSlide(currentSlide===0?3: (prev)=>prev-1)
           <img src={data[0]} alt="imgone"  loading="priority"/>
           <img src={data[1]} alt="imgone" />
           <img src={data[2]} alt="imgone"  />
+          
 
 
         </div>
